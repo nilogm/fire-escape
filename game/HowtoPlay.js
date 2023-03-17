767c3c0b667eac72dc9d6f7d185f33a189dc5483
braga-bilads <gabriel.b.ladislau@gmail.com>
2023-03-17 17:57:52 -0300

    How to play

--- game/HowtoPlay.js
+++ game/HowtoPlay.js
@@ -4,29 +4,24 @@ class HowtoPlay extends Phaser.Scene{
     }
     
     preload(){
-        this.load.image('main','./assets/mainscreen.png')
+        this.load.image('howtoplay','./assets/howtoplay.png')
     }
 
     create(){
         
-        var background = this.add.image(400, 400,'main')
+        var background = this.add.image(400, 400,'howtoplay')
         background.x = background.displayWidth/2
         background.y = background.displayHeight/2
 
-        this.backButton = this.add.text(400, 450, "Voltar")
+        this.backButton = this.add.text(200, 550, "Voltar")
                             .setOrigin(0.5)
                             .setPadding(10)
                             .setStyle({ backgroundColor: '#111' })
                             .setInteractive({hitArea:[20,20], useHandCursor: true })
                             .on('pointerdown', () => this.showInfo())
-                            .on('pointerover', () => this.infoButton.setStyle({ fill: '#f39c12' }))
-                            .on('pointerout', () => this.infoButton.setStyle({ fill: '#FFF' }));
-                            
-        this.add.text(150,100,"<nome> é um jogo em que você não pode ficar parado!\nO prédio está ruindo, e para fugir você deve correr!\nPegar a chave que abre a saída evitando obstáculos\n e Usa-la para sair pela porta").setStyle({ backgroundColor: '#111' })
-        this.add.text(150,170,"Seu objetivo é conseguir chegar o mais longe possível,\ncada andar descido conta um ponto no seu score!\nCada andar contém uma saida em um lugar aleatório nas laterais.")
-        this.add.text(150,220,"Em cada nível itens irão cair para te ajudar na aventura!").setStyle({ backgroundColor: '#112' })
-        this.add.text(150,250,"O Medkit irá previnir que você morra ao ser atingido uma vez!\nO Extintor pode ser usado para destruir obstaculos no seu caminho!\nO Machado pode ser usado para saír do andar sem a chave quebrando a porta!")
-        this.add.text(150,300,"Use as setinhas para se mover, e o espaço para usar o machado ou o extintor.")
+                            .on('pointerover', () => this.backButton.setStyle({ fill: '#f39c12' }))
+                            .on('pointerout', () => this.backButton.setStyle({ fill: '#FFF' }));
+                    
     
     }
 
